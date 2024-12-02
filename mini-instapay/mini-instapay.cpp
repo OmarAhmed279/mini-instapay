6a058f1e3fc574ad8946ca3f3c4aa91b94da130a
Wafaeyy <youssefmohamedwafaey@gmail.com>
2024-12-03 00:21:51 +0200

    isa el a5eer

--- mini-instapay/mini-instapay.cpp
+++ mini-instapay/mini-instapay.cpp
@@ -4,35 +4,27 @@
 #define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING 1 //this is so it doesn't crash
 
 #include "mini-instapay.h" //this has all the header files
-
 using namespace std;
 
-// Variables intialization
-
-int user_count;
-
-// Function intialization
+void land_page();
 void create_user();
 void user_login();
 void main_menu();
 void copydatatodatabase();
 void copydatabasetoapp();
 
+// create user structure
+struct user {
+    string  name, email, password, ContactInformation, linked_bank_accounts[3];
+    int id;
+};
 // create transcations structure
-struct trans {
+struct transcations {
     string  SenderAccount, RecieverAccount, date;
     int id;
     //if status = {success = 1, pending = 0, failed = -1}
     int ammount = 0, status;
 };
-// create user structure
-struct user {
-    string  name, email, password, ContactInformation, linked_bank_accounts[3];
-    int id;
-    trans usertrans;
-};
-
-
 // create admin structure
 struct admin {
     string id, name, email;
@@ -45,41 +37,57 @@ struct admin {
 
 int main()
 {
+    land_page();
+
+    return 0;
+}
+
+void land_page() {
+    // include namespace nana 
     using namespace nana;
-
-    //Initial Database Creation Code (One time run only)
-
-
-
-    //Define a form.
-    form fm;
-
-    //Define a label and display a text.
-    label lab{ fm, "Hello, Please Sign in." };
-    lab.format(true);
-
-    textbox txt{ fm, rectangle(0,0, 100,100), true }; //an empty textbox for username TODO: learn how to layout elements
-
-    //Define a button and answer the click event.
-    button btn{ fm, "Sign in" };
-    btn.events().click([&fm]() {
-        fm.close();
-        }); //currently this just closes the app
-
-    //Layout management
-    fm.div("vert <><<><weight=50% text><>><><weight=24<><button><>><>");
-    fm["text"] << lab;
-    fm.collocate();
-
-    //Show the form
-    using namespace nana;
+    // create form aka window, allign it to center with the size (800,600)
+    form fm{ API::make_center(800,600) };
+    // set background color to the form
+    fm.bgcolor(color(255, 165, 0));
+    // set the name of the window
+    fm.caption("InstaPay");
+    //close resize attribute
+    API::effects_edge_nimbus(fm, effects::edge_nimbus::none);
+    //create a label for each email and password textboxes
+    label lbl1{ fm,"Email:" }, lbl2{ fm,"Password:" };
+    // positioning
+    lbl1.move(rectangle(250, 150, 110, 17));
+    lbl2.move(rectangle(250, 250, 110, 17));
+    //create textboxes to input email and password
+    textbox txt1{ fm,rectangle(250, 170, 300, 30) }, txt2{ fm,rectangle(250, 270, 300, 30) };
+    // enable user input
+    txt1.editable(true);
+    txt2.editable(true);
+    // set font (optional)
+    txt1.typeface(paint::font("Arial", 12));
+    txt2.typeface(paint::font("Arial", 12));
+    // disable mutlilines
+    txt1.multi_lines(false);
+    txt2.multi_lines(false);
+    // hide password
+    txt2.mask('*');
+    // create signup button
+    button signup{ fm,"SignUp" };
+    signup.move(rectangle(250, 330, 100, 30));
+    // incase of click event it switches to create user page
+    signup.events().click([] {create_user(); });
+    // login button
+    button login{ fm,"Login" };
+    login.move(rectangle(450, 330, 100, 30));
+    login.events().click([] {user_login(); });
+    // show form
     fm.show();
-    //Start to event loop process, it blocks until the form is closed.
+    // switch control from main to nana then back to main when you close the form
     exec();
 }
 void create_user()
 {
-    // type here
+    cout << "Omak Ar3a!! HEHE" << endl;
 }
 
 void user_login()
